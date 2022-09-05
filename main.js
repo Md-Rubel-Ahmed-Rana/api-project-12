7248e4cabf7ae0fe1d4d0e401f0ab84ed77ee47c
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-05 16:35:21 +0600

    Showing All Surah name, number and Ayah is completed

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const loadQuran = async() => {
     const url = await `https://api.alquran.cloud/v1/quran`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.surahs);
+    displayQuran(data.data.surahs);
 };
 
+
+// display quran to the UI
+const displayQuran = async (surahs) =>{
+    // select Quran container
+    const quranContainer = document.getElementById("quran-container");
+    surahs.map((surah) => {
+        const quranDiv = document.createElement("div");
+        quranDiv.classList.add("col");
+        quranDiv.innerHTML = `
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="card-title fw-bold">Name: ${surah.name} </h5>
+                    <div class="d-flex justify-content-around">
+                        <p class="card-text"><b>Serial: ${surah.number}</b></p>
+                        <p class="card-text"><b>Ayah: ${surah.ayahs.length}</b></p>
+                    </div>
+                </div>
+            </div>
+        `;
+        quranContainer.appendChild(quranDiv);
+    })
+}
 
 
 loadQuran();
